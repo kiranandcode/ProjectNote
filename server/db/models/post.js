8491b59c6e3195436cbf45ed0b710e2016c10398
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2017-10-31 20:24:50 +0000

    api done

--- server/db/models/post.js
+++ server/db/models/post.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 mongoose.Promise =  Promise;
 
 const postSchema = new Schema({
-    person: {type: Number, ref: 'Person', required: true},
+    person: {type: Schema.Types.ObjectId, ref: 'User', required: true},
     message: { type: String, required: true },
     date: { type:Date, default: Date.now, required: true}
 });
--- server/routes/project.js
+++ server/routes/project.js
@@ -3,9 +3,10 @@ module.exports = (env) => {
     const router = express.Router();
     const User = require('../db/models/user');
     const Project = require('../db/models/project');
+    const Post = require('../db/models/post');
 
-    router.use((req,res,next) => {
-        if(!req.user) {
+    router.use((req, res, next) => {
+        if (!req.user) {
             res.send(403, 'unauthorized');
         } else {
             next();
@@ -16,7 +17,7 @@ module.exports = (env) => {
         const user_id = req.user._id;
         console.log(req.user);
         console.log(user_id);
-        Project.find({people: user_id}).exec().then((data) => {
+        Project.find({ people: user_id }).exec().then((data) => {
             // TODO ; sanitize data
             res.json(data);
         }).catch(err => {
@@ -25,16 +26,16 @@ module.exports = (env) => {
         });
     });
 
-    router.post('/new', (req,res) => {
+    router.post('/new', (req, res) => {
         const user_id = req.user._id;
         const { project_name } = req.body;
 
         Project.findOne({ 'name': project_name }, (err, projectMatch) => {
-            if(projectMatch){
+            if (projectMatch) {
                 return res.json({
                     error: 'Sorry, already exists project with name ' + project_name
                 });
-            } 
+            }
 
             const newProject = new Project({
                 'name': project_name,
@@ -43,7 +44,7 @@ module.exports = (env) => {
             });
 
             newProject.save((err, savedProject) => {
-                if(err) res.json({error: err});
+                if (err) res.json({ error: err });
                 else return res.json(savedProject);
             });
         });
@@ -53,52 +54,87 @@ module.exports = (env) => {
 
 
 
-    router.use('/:id', (req,res,next) => {
-        
+    router.use('/:id', (req, res, next) => {
+
         const project_id = req.params.id;
         const user_id = req.user._id;
-        Project.findOne({_id:project_id, people: user_id}).exec().then((project) => {
-            if(!project) {
+        Project.findOne({ _id: project_id, people: user_id }).exec().then((project) => {
+            if (!project) {
                 res.send(403, 'unauthorized');
             } else {
                 next();
             }
         }).catch((err) => {
-            res.json({error: err});
+            res.json({ error: err });
         });
     });
 
-    router.delete('/:id', (req,res,next) => {
+    router.delete('/:id', (req, res, next) => {
         const project_id = req.params.id;
         const user_id = req.user._id;
 
-        Project.findOneAndRemove({_id: project_id, people: user_id}).exec().then((removed) => {
-            res.json({removed: removed.name});
+        Project.findOneAndRemove({ _id: project_id, people: user_id }).exec().then((removed) => {
+            res.json({ removed: removed.name });
         }).catch(err => {
-            res.json({error: err});
+            res.json({ error: err });
         });
     })
 
-    router.get('/:id/posts', (req,res,next) => {
+    router.get('/:id/posts', (req, res, next) => {
         const project_id = req.params.id;
         const user_id = req.user._id;
 
         Project.findOne({
             _id: project_id,
             people: user_id
-        }).populate({
+        }).select('-people -__v').populate({
             path: 'posts',
+            select: '-__v',
             populate: {
-                path: 'people'
+                path: 'person',
+                select: '-local.password -__v -projects'
             }
-        }).exec().then((data) => {
+       }).exec().then((data) => {
             // TOODO sanitize data
             res.json(data);
         });
     });
 
+    router.post('/:id/posts/add', (req, res, next) => {
+        const project_id = req.params.id;
+        const user_id = req.user._id;
 
-    router.get('/:id/users', (req,res,next) => {
+        let { message } = req.body;
+        // TODO sanitize message
+        message = JSON.stringify(message);
+        
+        const newPost = Post({
+            person: user_id,
+            message: message
+        });
+        newPost.save((err, post) => {
+            if(err) {
+                res.json({error: err});
+            } else if(!post) {
+               res.json({error: 'Unknown error occurred'});
+            } else {
+                Project.update({
+                    _id: project_id,
+                    people: user_id
+                }, {$push: {posts: post._id}}).then(data => {
+                    res.json(data);
+                }).catch(err => {
+                    res.json({error: err});
+                });
+            }
+        });
+
+   });
+
+
+
+
+    router.get('/:id/users', (req, res, next) => {
 
         const project_id = req.params.id;
         const user_id = req.user._id;
@@ -113,11 +149,11 @@ module.exports = (env) => {
         }).exec().then((data) => {
             res.json(data.people);
         }).catch(err => {
-            res.json({error: err});
+            res.json({ error: err });
         });
     });
 
-    router.post('/:id/users/add', (req,res,next) => {
+    router.post('/:id/users/add', (req, res, next) => {
         const project_id = req.params.id;
         const self_user_id = req.user._id;
 
@@ -129,29 +165,29 @@ module.exports = (env) => {
 
             Project.findOne({ _id: project_id, people: user_id }).exec().then((data) => {
                 console.log(data);
-                if(!data) {
-                Project.update({ _id: project_id}, {$push: { people: user_id}}).then((data) => {
-                    res.json(data);
-                }).catch(err => {
-                    res.json({error: err});
-                });
+                if (!data) {
+                    Project.update({ _id: project_id }, { $push: { people: user_id } }).then((data) => {
+                        res.json(data);
+                    }).catch(err => {
+                        res.json({ error: err });
+                    });
                 } else {
-                    res.json({error: 'User already member of the project'});
+                    res.json({ error: 'User already member of the project' });
                 }
             }).catch(err => {
-                res.json({error: err});
-           });
+                res.json({ error: err });
+            });
 
         }).catch(err => {
             console.log(err);
-            res.json({error: 'User by that userid does not exist on the system'});
+            res.json({ error: 'User by that userid does not exist on the system' });
         });
 
-   });
+    });
 
 
 
-    router.delete('/:id/users/:user_id', (req,res,next) => {
+    router.delete('/:id/users/:user_id', (req, res, next) => {
         const project_id = req.params.id;
         const self_user_id = req.user._id;
         const user_id = req.params.user_id;
@@ -163,25 +199,25 @@ module.exports = (env) => {
 
             Project.findOne({ _id: project_id, people: user_id }).exec().then((data) => {
                 console.log(data);
-                if(data) {
-                Project.update({ _id: project_id}, {$pull: { people: user_id}}).then((data) => {
-                    res.json(data);
-                }).catch(err => {
-                    res.json({error: err});
-                });
+                if (data) {
+                    Project.update({ _id: project_id }, { $pull: { people: user_id } }).then((data) => {
+                        res.json(data);
+                    }).catch(err => {
+                        res.json({ error: err });
+                    });
                 } else {
-                    res.json({error: 'User is not a member of the project'});
+                    res.json({ error: 'User is not a member of the project' });
                 }
             }).catch(err => {
-                res.json({error: err});
-           });
+                res.json({ error: err });
+            });
 
         }).catch(err => {
             console.log(err);
-            res.json({error: 'User by that userid does not exist on the system'});
+            res.json({ error: 'User by that userid does not exist on the system' });
         });
 
-   });
+    });
 
 
 
@@ -191,5 +227,5 @@ module.exports = (env) => {
 
 
 
-     return router;
+    return router;
 }
