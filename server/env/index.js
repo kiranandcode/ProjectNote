b2587b1bb3d661197b2415930357451f94933be9
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2017-10-31 11:09:28 +0000

    added secret developer environment setup script

--- server/env/index.js
+++ server/env/index.js
@@ -4,13 +4,17 @@ const production = process.env.NODE_ENV === 'production';
 const google_client_id = process.env.GOOGLE_CLIENT_ID;
 const google_client_secret = process.env.GOOGLE_CLIENT_SECRET;
 const mongo_db_url = 'mongodb://' + process.env.DBNAME + ":" + process.env.KEY + "@" + process.env.DBNAME + ".documents.azure.com:" + process.env.COSMOSPORT + "/?ssl=true";
+const mongo_db_local_url = process.env.MONGODB_LOCAL || mongo_db_url;
 
-module.exports = {
+const env = {
     PORT: port,
     SECRET: secret,
     PRODUCTION: production,
     GOOGLE_CLIENT_ID: google_client_id,
     GOOGLE_CLIENT_SECRET: google_client_secret,
     MONGODB_URL: mongo_db_url,
-    MONGODB_DEV_URL: ''
+    MONGODB_DEV_URL: mongo_db_local_url
 };
+
+console.log(JSON.stringify(env));
+module.exports = env;
