250d3bc7d57105d5eaeda61ebad78cf8e89c6f29
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2017-11-01 07:24:05 +0000

    implemented adding messge functionality

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -12,7 +12,8 @@ class Project extends Component {
             id: props.project_id,
             posts: [],
             message: '',
-            authorized: false
+            authorized: false,
+            errors: []
         };
 
         this.getposts = this.getposts.bind(this);
@@ -38,11 +39,12 @@ class Project extends Component {
                     authorized: true
                 });
             }).catch(err => {
-                console.log("Got an error");
+                console.log("Got an error" + err);
                 this.setState({
                     posts: [],
                     id: new_id,
-                    authorized: false
+                    authorized: false,
+                    errors: []
                 });
             });
         } else {
@@ -52,7 +54,47 @@ class Project extends Component {
             });
         }
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+
+    handleSubmit(event) {
+        event.preventDefault();
+        if(this.state.message) {
+        axios.post(env.root + '/api/project/' + this.props.project_id + '/posts/add', { message: this.state.message }).then(response => {
+            if (response.data && !response.data.error) {
+                this.setState({
+                    message: '',
+                    errors: []
+                });
+                this.getposts(this.props);
+            } else if (response.data && response.data.error) {
+                this.setState({
+                    errors: [JSON.stringify(response.data.eror)]
+                });
+            } else {
+                this.setState({
+                    errors: ['Unknown error occurred']
+                });
+            }
+        }).catch(err => {
+            this.setState({
+                errors: [JSON.stringify(err)]
+            });
+        });
+    } else {
+        this.setState({
+            errors: ['Can not have a post with no message']
+        });
+    }
     }
 
     componentDidMount() {
@@ -74,14 +116,34 @@ class Project extends Component {
                 return (
                     <div className="Home">
                         <h3>DevLog for Project</h3>
+                        <form className="PostForm">
+                            <label htmlFor="message">Update: </label>
+                            <input type="text" name="message" value={this.state.message} onChange={this.handleChange} />
+                            <button onClick={this.handleSubmit}>Send</button>
+                        </form>
+                        {this.state.errors.length !== 0 &&
+                            (<div><h4>Errors</h4>
+                                <ul>
+                                    {this.state.errors.map((err) => (
+                                        <li>
+                                            <pre>
+                                                {JSON.stringify(err)}
+                                            </pre>
+                                        </li>
+                                    ))}
+                                </ul>
+                            </div>
+                            )
+
+                        }
 
                         <ul style={{ listStyle: 'none' }}>
                             {this.state.posts.map(post => (
-                                    <li>
-                                        <p><strong>{post.person.local.username}</strong> - {post.message}/{post.date}</p>
-                                    </li>
+                                <li>
+                                    <p><strong>{post.person.local.username}</strong> - {post.message}/{post.date}</p>
+                                </li>
                             ))}
-                       </ul>
+                        </ul>
                     </div>
                 );
             } else {
