d4d720b0a0b2eaa3bb8e2f54ad66521460803d20
Gopiandcode(Kiran Gopinathan) <zcabkgo@ucl.ac.uk>
2017-11-03 05:54:09 +0000

    added graceful shutdowns to app

--- server/db/index.js
+++ server/db/index.js
@@ -1,15 +1,15 @@
 module.exports = (env) => {
     const mongoose = require('mongoose');
     mongoose.Promise = global.Promise;
-
-
-    if(env.PRODUCTION) {
+    let gracefulShutdown;
+    if (env.PRODUCTION) {
         mongoose.connect(env.MONGODB_URL, { useMongoClient: true });
     } else {
-        mongoose.connect(env.MONGODB_DEV_URL, {useMongoClient: true});
+        mongoose.connect(env.MONGODB_DEV_URL, { useMongoClient: true });
     }
 
     const db = mongoose.connection;
+
 
     db.on('error', err => {
         console.log("There was an error connecting to the database: " + err);
@@ -18,6 +18,34 @@ module.exports = (env) => {
     db.once('open', () => {
         console.log("Successfully connected to the mongoDB database");
     });
+
+
+   gracefulShutdown = function (msg, callback) {
+        mongoose.connection.close(function () {
+            console.log('Mongoose disconnected through ' + msg);
+            callback();
+        });
+    };
+    // For nodemon restarts
+    process.once('SIGUSR2', function () {
+        gracefulShutdown('nodemon restart', function () {
+            process.kill(process.pid, 'SIGUSR2');
+        });
+    });
+    // For app termination
+    process.on('SIGINT', function () {
+        gracefulShutdown('app termination', function () {
+            process.exit(0);
+        });
+    });
+    // For Heroku app termination
+    process.on('SIGTERM', function () {
+        gracefulShutdown('Heroku app termination', function () {
+            process.exit(0);
+        });
+    });
+
+
 
     return db;
 }
